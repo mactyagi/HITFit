9ca3fdef4880c3cbe0ef6d6c674130da2751f41a
Manu <mactyagi@icloud.com>
2024-07-24 23:31:44 +0530

    Initial Commit

---
+++ HITFit/HITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HITFitApp.swift
+//  HITFit
+//
+//  Created by Manukant Tyagi on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
